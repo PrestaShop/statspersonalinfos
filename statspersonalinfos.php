280b9ed0c7a6d5adb075400a817795839e3028ec
Michel ANTOINE <michel.br.antoine@gmail.com>
2017-01-10 14:17:49 +0100

    Update minimum compliancy version
    (cherry picked from commit 46e9cde530250e6a17beadb216276899ce2704d6)

--- statspersonalinfos.php
+++ statspersonalinfos.php
@@ -45,7 +45,7 @@ class statspersonalinfos extends ModuleGraph
 
         $this->displayName = $this->trans('Registered customer information', array(), 'Modules.Statspersonalinfos.Admin');
         $this->description = $this->trans('Adds information about your registered customers (such as gender and age) to the Stats dashboard.', array(), 'Modules.Statspersonalinfos.Admin');
-        $this->ps_versions_compliancy = array('min' => '1.7.0.0', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
     }
 
     public function install()
